49b36c5df1b84712d12d59eaaf9d7e4500e8b988
Renan Pupin <renan.pupin@gmail.com>
2018-04-17 16:54:00 -0300

    improve repository

--- backend/app/repositories/user.js
+++ backend/app/repositories/user.js
@@ -2,18 +2,10 @@ const mongooseUtils = require("../utils/mongoose");
 
 const UserSchema = require("../models/user");
 
+//TODO: let use default generic routes (REST)
+
 module.exports = {
     findOne: async ( query, populate = '' ) => {
-      try{
-        let user = await mongooseUtils.findOne( UserSchema )( query, populate );
-
-        if (user) {
-          return user;
-        }else{
-            throw new Error("User not found.");
-        }
-      }catch(e){
-          return e;
-      }
+      return await mongooseUtils.findOne( UserSchema )( query, populate );
     },
 }
